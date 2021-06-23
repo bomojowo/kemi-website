65330ad3b8b28a122116ef01734bac100120b9f7
Bukola Omojowo <bomojowo@gmail.com>
2021-06-23 11:21:42 +0100

    Changed content

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <h1>Inspiirae</h1>
+        <h3>Nav Bar goes here</h3>
+        </header>
+        <main>
+          Picture
+        </main>
+        <footer>
+          links to socials
+        </footer>
+          
+      
     </div>
   );
 }

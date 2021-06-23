855e6a0f5bb4300627a7659b35815fa6842bf4a1
Bukola Omojowo <bomojowo@gmail.com>
2021-06-23 14:58:34 +0100

    changed file extension on all imaes from .PNG to .png

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,8 +1,15 @@
+import PicOne from '../images/one.png'
+import PicTwo from '../images/two.png'
+import PicThree from '../images/three.png'
+import PicFour from '../images/four.png'
 
 function Carousel(): JSX.Element {
     return(
         <div>
-            carousel pics go here
+            <img src={PicOne} alt="quote" />
+            <img src={PicTwo} alt="quote" />
+            <img src={PicThree} alt="quote" />
+            <img src={PicFour} alt="quote" />
         </div>
     )
 }

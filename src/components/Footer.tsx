835f43bf3777cc9b3ba3f413950d3ed69554e65b
Bukola Omojowo <bomojowo@gmail.com>
2021-06-25 14:53:46 +0100

    Added Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+function Footer(): JSX.Element {
+  return (
+    <div className="footer">
+     link to socials
+     <a href="https://instagram.com/inspiirae?utm_medium=copy_link" rel="noreferrer" target="_blank"><i className="fab fa-instagram"></i></a>
+    </div>
+  );
+}
+
+export default Footer;
